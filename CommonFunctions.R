abaafa80018ed9de54f3d454cb3eb48cc5a1a9ea
Yoan G. Hermida <43450199+yoanhermida@users.noreply.github.com>
2025-05-08 08:40:05 -0400

    Create CommonFunctions.R

---
+++ CommonFunctions.R
@@ -0,0 +1,43 @@
+# install a package
+install.packages("packageName")
+
+# load a package
+library(packageName)      
+
+# get help
+help(functionName)
+
+# create a data frame
+value1 = "some value"
+value2 = "another value"
+value3 = "last value"
+myDataFrame = (value1)
+
+# get current working directory
+getwd()
+
+# set working directory
+setwd("/path/to/directory/")
+
+# find variables within a dataset
+grep("variableName", names(datasetName))
+
+# function that consumes a spreadsheet and runs simple linear regression against the data
+# file_path should be /something/like/this.xlms
+# dv = dependent variable
+# iv = independent variable
+# control = control
+simple_regression_spreadsheet <- function(file_path, dv, iv, control) {
+
+  # it is not best practice to load packages this way
+  # but i'm doing it anyway
+  # this assumes you've already install.packages("readxl")
+  library(readxl)
+
+  print("\nReading spreadsheet and converting it into a dataframe.\n")  
+  df <- read_excel(file_path)
+
+  print("\nRegression results.\n")
+  model <- lm(dv~iv+control, data=df)
+  summary(model)
+}
